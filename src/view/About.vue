b975b7a61d002d957de1a209f63549889b35895b
LucasBPalacios <luccaspalacios2003@gmail.com>
2023-11-17 10:39:40 -0300

    feature(intro_vuejs): aplicação para retorno de informações através do cep

---
+++ src/view/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Sobre o app</h1>
+        <p>Desenvolvido por Lucas Palácios</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
